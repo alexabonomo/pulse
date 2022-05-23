4a6e1f2195e5b8963ba42d086205c3529486f8b1
alexabonomo <me@alexaann.net>
2022-05-23 16:41:40 -0700

    pulse update to maddmapper layout

--- sketch.js
+++ sketch.js
@@ -1,54 +1,104 @@
-var serial;
-var portName = '/dev/cu.usbmodem1421';
-var circleSize = 10;
-var heartRate;
 
-// Setup only runs once 
+let serial;
+let latestData = "waiting for data";
+
+let img;
+
+function preload() {
+  img_r = loadImage('data/seraphim_r.png');
+  img_g = loadImage('data/seraphim_g.png');
+  img_b = loadImage('data/seraphim_b.png');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  serial = new p5.SerialPort();
-  serial.on('list', printList);  // set a callback function for the serialport list event
-  serial.on('connected', serverConnected); // callback for connecting to the server
-  serial.on('open', portOpen);        // callback for the port opening
-  serial.on('data', serialEvent);     // callback for when new data arrives
-  serial.on('error', serialError);    // callback for errors
-  serial.on('close', portClose);
+  frameRate(60);
+ createCanvas(windowWidth, windowHeight);
 
-  serial.list();
-  serial.open(portName);
+ serial = new p5.SerialPort();
+
+ serial.list();
+ serial.open('COM3');
+
+ serial.on('connected', serverConnected);
+
+ serial.on('list', gotList);
+
+ serial.on('data', gotData);
+
+ serial.on('error', gotError);
+
+ serial.on('open', gotOpen);
+
+ serial.on('close', gotClose);
 }
 
 function serverConnected() {
-  print('connected to server.');
+ print("Connected to Server");
 }
 
-function portOpen() {
-  print('the serial port opened.')
+function gotList(thelist) {
+ print("List of Serial Ports:");
+
+ for (let i = 0; i < thelist.length; i++) {
+  print(i + " " + thelist[i]);
+ }
 }
 
-function serialEvent() {
-  heartRate = serial.read();
-  circleSize = heartRate;
+function gotOpen() {
+ print("Serial Port is Open");
 }
 
-function serialError(err) {
-  print('Something went wrong with the serial port. ' + err);
+function gotClose(){
+ print("Serial Port is Closed");
+ latestData = "Serial Port is Closed";
 }
 
-function portClose() {
-  print('The serial port closed.');
+function gotError(theerror) {
+ print(theerror);
 }
 
-function printList(portList) {
-    for(var i = 0; i < portList.length; i++) {
-        print(i + " " + portList[i]);
-    }
+function gotData() {
+ let currentString = serial.readLine();
+  trim(currentString);
+ if (!currentString) return;
+ //console.log(currentString);
+ latestData = currentString;
 }
 
-// Draws in the browser
 function draw() {
-  background("#85144b");
-  fill("#FF851B");
-  noStroke();
-  ellipse(width/2, height/2, circleSize, circleSize);
+blendMode(NORMAL);
+ background(0);
+ fill(0,0,0);
+ text(latestData, 10, 10);
+ // Polling method
+ 
+
+makeImg(img_r, img_g, img_b, 100, 100);
+makeImg(img_r, img_g, img_b, 500, 100);
+makeImg(img_r, img_g, img_b, 900, 100);
+makeImg(img_r, img_g, img_b, 100, 400);
+makeImg(img_r, img_g, img_b, 500, 400);
+makeImg(img_r, img_g, img_b, 900, 400);
+
+
+
+
+
+
+}
+
+function makeImg(r, g, b, x , y){
+
+ let hr = latestData/10
+ const wid = 350;
+ const len = 250;
+
+ let dx = x+(hr);
+ let dy = y+(hr);
+
+ blendMode(SCREEN);
+ image(r, dx, y, wid, len);
+ image(g, x, dy, wid, len);
+ image(b, dx, dy, wid, len);
+
 }
